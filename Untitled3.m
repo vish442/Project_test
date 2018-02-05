c333b367cff8b6987eee6f51f77cd10a29fff79f
vish442 <vish9688@gmail.com>
2018-02-05 14:46:52 +0000

    Changes test

--- Untitled3.m
+++ Untitled3.m
@@ -1,7 +1,17 @@
-n=10; % number of lines
-a= inputdlg('enter x coordinates','test',n)
-x=str2num(a{1})
-b= inputdlg('enter y coordinates','test',n)
-y=str2num(b{1})
-c= inputdlg('enter z coordinates','test',n)
-z=str2num(c{1})
+% n=10; % number of lines
+% a= inputdlg('enter x coordinates','test',n)
+% x=str2num(a{1})
+% b= inputdlg('enter y coordinates','test',n)
+% y=str2num(b{1})
+% c= inputdlg('enter z coordinates','test',n)
+% z=str2num(c{1})
+prf = 5;                                                                %creating a rectanglaur pulse of 1 sec interval with 10ms width
+pulseWidth = 10e-3;
+pulseBandwidth = 1/pulseWidth;
+fs = 2*pulseBandwidth;
+wav = phased.RectangularWaveform('PRF',prf,'PulseWidth',pulseWidth,...
+  'SampleRate',fs);
+channel.SampleRate = fs;
+x = wav();
+plot(real(x)); title('Waveform output, real part');
+xlabel('Samples'); ylabel('Amplitude (V)');
