66a590d2664327bf6ef3c9245fff19c63fa9b646
vish442 <vish9688@gmail.com>
2018-02-07 00:39:25 +0000

    Fixes

--- Untitled3.m
+++ Untitled3.m
@@ -1,17 +1,6 @@
-% n=10; % number of lines
-% a= inputdlg('enter x coordinates','test',n)
-% x=str2num(a{1})
-% b= inputdlg('enter y coordinates','test',n)
-% y=str2num(b{1})
-% c= inputdlg('enter z coordinates','test',n)
-% z=str2num(c{1})
-prf = 5;                                                                %creating a rectanglaur pulse of 1 sec interval with 10ms width
-pulseWidth = 10e-3;
-pulseBandwidth = 1/pulseWidth;
-fs = 2*pulseBandwidth;
-wav = phased.RectangularWaveform('PRF',prf,'PulseWidth',pulseWidth,...
-  'SampleRate',fs);
-channel.SampleRate = fs;
-x = wav();
-plot(real(x)); title('Waveform output, real part');
-xlabel('Samples'); ylabel('Amplitude (V)');
+  antenna = phased.IsotropicAntennaElement;
+  radiator = phased.Radiator('Sensor',antenna,...
+              'OperatingFrequency',300e6);
+  x = [1;1];
+  radiatingAngle = [30 10]';
+  y = radiator(x,radiatingAngle);
--- Untitled4.m
+++ Untitled4.m
@@ -1,14 +1,4 @@
-clf
-TxPos=[50 60 70;99 50 1000;100 450 230]
-% b=TxPos1(:,end)
-% hold on
-hold on
-for k=1:length(TxPos)
-    A=TxPos(k,:)
-    A(1)
-    A(2)
-    A(3)
-%     TxPos(1:2)
-    PlotPaths([A(1); A(2); -A(3)],[50; 60 ; -70], ...
-    5000,5)
-end
+  projector = phased.IsotropicProjector('FrequencyRange',[1e3 30e3]);
+  fc = 20e3;
+  patternAzimuth(projector,fc,10);
+%
