814788076e7eeef476dd4d8450214b4df1fdec15
vish442 <vish9688@gmail.com>
2018-02-20 13:36:47 +0000

    Matlabwebsitehelp

--- Toolprogram.m
+++ Toolprogram.m
@@ -5,7 +5,7 @@ fc=input(fc);
 channelDepth =2000
 Numberofsourcepaths =1
 BottomLoss=0.5
-LossFrequencies=99999999:99999999+1
+LossFrequencies=1:100
 VoltageSensitivity=-200
 % xcorr='Please enter the x cordinate of the receiver';
 xcorr=0
@@ -13,40 +13,22 @@ xcorr=0
 ycorr=0
 % zcorr='Please enter the z cordinate of the receiver';
 zcorr=0
-Speed='What is your propagation speed?';
 Speed=1500
 forstep=5
 maxdistance=15500
-% values=zeros(size(xcorr))
 for xcorr=14500:forstep:maxdistance
     
     isopath= phased.IsoSpeedUnderwaterPaths(...    %Creates a  channel for the propagation 
           'ChannelDepth',channelDepth,...
-          'NumPathsSource',...                  %
-          'Property',...                        %   Property where the default is 'Auto'. When you set this
-          'NumPaths',Numberofsourcepaths,...    %   property to 'Auto', the object automatically determines the
+          'NumPathsSource',...                  
+          'Property',...                        
+          'NumPaths',Numberofsourcepaths,...    
           'PropagationSpeed',Speed,...
-          'BottomLoss',BottomLoss,...           %   you set this property to 'Property', the number of paths is
-          'TwoWayPropagation',false,...         %   specified via the NumPaths property
-          'LossFrequencies',LossFrequencies);   %   number of paths based on spreading and reflection losses. When
+          'BottomLoss',BottomLoss,...           
+          'TwoWayPropagation',false,...         
+          'LossFrequencies',LossFrequencies);   
     channel = phased.MultipathChannel(...
           'OperatingFrequency',fc);                                                
-%     length(TxPos)                                      
-%     ValueofA=cell(length(TxPos),1);                                            
-%     figure (1)    
-%     hold on %   are ignored and not plotted     
-%  for k=1:length(TxPos)  %for loop for each point of the transmitter
-%   A=TxPos(k,:)    
-%   [paths,dop,aloss] = isopath([A(1); A(2); -A(3)],[xcorr;ycorr;-zcorr],... %coordinates for transmitter and reciever,and velocity of transmitter and reciever
-%   [0;0;0],[0;0;0],1); 
-%   ValueofA{k}=aloss
-% %   data=ValueofA{k}
-% %   fnm=sprintf('file_%d.csv',k)
-% %   save(fnm,'data')       
-%   plot(1:Numberofsourcepaths,paths(1,:))
-%   xlabel('Path Index')
-%   ylabel('Delay Time (s)')                                          
-%   end
 
 %creating a rectanglaur pulse of 1 sec interval with 10ms width
     prf = 1;                 
@@ -58,32 +40,25 @@ for xcorr=14500:forstep:maxdistance
     channel.SampleRate = fs;
 
     projector = phased.IsotropicProjector(...                                    %set up the sound projector with frequency range of 0 to 30e3
-        'FrequencyRange',[99999999 99999999+1],'VoltageResponse',50,'BackBaffled',false);
-    % figure(20)
-    % patternAzimuth(projector,fc);
+        'FrequencyRange',[1 100],'VoltageResponse',250,'BackBaffled',false);
+    
     [ElementPosition,ElementNormal] = helperSphericalProjector(8,fc,Speed);
     projArray = phased.ConformalArray(...
         'ElementPosition',[ElementPosition],...
         'ElementNormal',[ElementNormal],'Element',projector);
 
-%     figure(23)
-%     pattern(projArray,fc,-180:180,0,'CoordinateSystem','polar',...
-%         'PropagationSpeed',Speed);
-    % figure(56)
-    % viewArray(projArray,'ShowNormals',true);
-
     projRadiator = phased.Radiator('Sensor',projector,...                   %radiates the sound projector signal outwards to the far field
     'PropagationSpeed',Speed,'OperatingFrequency',fc);
 
-    %takes only the last value of the coordinate at the moment
+    
     beaconPlat = phased.Platform('InitialPosition',[0; 0; 0],...   % set a platform for the sound projector
      'Velocity',[0; 0; 0]);
 
-    hydrophone = phased.IsotropicHydrophone('FrequencyRange',[99999999 99999999+1],...  %set up Hydrophone with the same frequency range as the sound projector and approiate voltage 
+    hydrophone = phased.IsotropicHydrophone('FrequencyRange',[1 1000],...  %set up Hydrophone with the same frequency range as the sound projector and approiate voltage 
      'VoltageSensitivity',VoltageSensitivity);
 
     array = phased.ULA('Element',hydrophone,...                             %   This object models a ULA formed with identical sensor elements.
-    'NumElements',3,'ElementSpacing',Speed/fc/2,...
+    'NumElements',2,'ElementSpacing',Speed/fc/2,...
     'ArrayAxis','x');
 
     arrayCollector = phased.Collector('Sensor',array,...                    %collects incident narrowband signals from given directions 
@@ -92,15 +67,10 @@ for xcorr=14500:forstep:maxdistance
     arrayPlat= phased.Platform('InitialPosition',[xcorr; ycorr; -zcorr],...
     'Velocity',[0; 0; 0]);
 
-    % rx = phased.ReceiverPreamp(...                                          %Amplifiy weak signals to easier processing
-    %     'Gain',20,...
-    %     'SampleRate',fs)
     x = wav(); 
-    % figure(3);
-    % plot(wav); title('Waveform output, real part');
-    % xlabel('Samples'); ylabel('Amplitude (V)');                                                %Transmit 10 pings, pings appear as a peak in the received signals
+    %Transmit 10 pings, pings appear as a peak in the received signals
     numTransmits = 10;
-    rxsig = zeros(size(x,1),3,numTransmits);
+    rxsig = zeros(size(x,1),2,numTransmits);
     for i = 1:numTransmits
 
     % Update array and acoustic beacon positions
@@ -139,20 +109,7 @@ for xcorr=14500:forstep:maxdistance
 end
 figure(5)
 plot(b1,a1)
-% area(b1(),a1(),'basevalue',0,'Facecolor','r')
-% area(b1,a1,'basevalue',190,'FaceColor','g')
-% area(A(1:13),B(1:13),'basevalue',0,'FaceColor','g');
+
 xlabel('Distance in x axis(m)')
 ylabel('Reciever level(dB)')
 'Tool finish'
-%     figure (2)
-%     clf
-%     hold on
-%     for k=1:length(TxPos)   %propgation path for each transmitter
-%         A=TxPos(k,:)
-%         A(1)
-%         A(2)
-%         A(3)
-%         PlotPaths([A(1); A(2); -A(3)],[xcorr;ycorr;-zcorr,],...
-%         [channelDepth],[Numberofsourcepaths]);
-%     end
